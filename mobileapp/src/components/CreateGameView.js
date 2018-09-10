89611ffac8910bbb301ba6947da0b23a8de8793b
yyypasserby <yiyunyao@live.com>
2018-09-10 00:20:56 -0700

    Add animation to create game view

--- mobileapp/src/components/CreateGameView.js
+++ mobileapp/src/components/CreateGameView.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import {
+  Animated,
+  StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 class CreateGameView extends React.Component {
+  state = {
+    bounceValue: new Animated.Value(100),
+  }
+
+  componentDidMount() {
+    Animated.spring(
+      this.state.bounceValue,
+      {
+        toValue: 0,
+        velocity: 3,
+        tension: 2,
+        friction: 8,
+      }
+    ).start();
+  }
+
   render() {
     return (
-      <View>
+      <Animated.View
+        style={[
+          styles.subView,
+          {transform: [{
+            translateY: this.state.bounceValue
+          }]}
+        ]}>
         <Text>CreateGameView</Text>
-      </View>
+      </Animated.View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  subView: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#FFFFFF',
+    height: 100,
+  },
+});
 
 export default CreateGameView;
